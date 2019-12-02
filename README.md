9095e6b588f1831bdac40417edd3a9721050c29c
Akuma Isaac Akuma <akumaisaacakuma@gmail.com>
2019-12-02 16:56:32 +0100

    few doc and test update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # deduplicate
 
-[![Build Status](https://travis-ci.com/Akumzy/deduplicate.svg?branch=master)](https://travis-ci.com/Akumzy/deduplicate) 
+[![Build Status](https://travis-ci.com/Akumzy/deduplicate.svg?branch=master)](https://travis-ci.com/Akumzy/deduplicate)
 [![Coverage Status](https://coveralls.io/repos/github/Akumzy/deduplicate/badge.svg?branch=master)](https://coveralls.io/github/Akumzy/deduplicate?branch=master)
 
 ⚠️ Documentaion is very much incomplete but you can always checkout the test file for usage guide.
@@ -19,28 +19,34 @@ This package provides all the necessaries utility functions to implements file d
 
 ## Usage
 
-
-
-- `deduplicate` a function that is used for creating file blocks and hash
 ```ts
-deduplicate(path: string, chunk?: number, algorithm?: string): Promise<Dedupe.HashObject>
+import Dedupe from '@akumzy/dd`
+
 ```
 
-**parameters:**
-- path: The file absolute path.
+- `Dedupe.deduplicate` is used for creating file blocks and hash.
+
+```ts
+Dedupe.deduplicate(
+    path: string,
+    chunk?: number,
+    algorithm?: string
+): Promise<Dedupe.HashObject>
+```
+
+- path: File absolute path.
 - chunk: Block size in bytes
   - default: `4 * 1024 * 1024` (4mb)
 - algorithm: Algorithm to use for hash file.
   - default: `sha256`
 
-
 **Example**
 
 ```js
-const ONE_MAGA_BYTES = 1024 * 1024
-let filePath = 'path-to-file'
-let info = await deduplicate(filePath, ONE_MAGA_BYTES)
-console.log(info)
+const ONE_MAGA_BYTES = 1024 * 1024;
+let filePath = "path-to-file";
+let info = await deduplicate(filePath, ONE_MAGA_BYTES);
+console.log(info); // =>
 //  {
 //     hash: 'f27a663ef8df8091e94d07ba090449a34b68461b9af5557377423057ce902484',
 //     blocks: [
@@ -54,6 +60,20 @@ console.log(info)
 //   }
 ```
 
+- `Dedupe.createBlocks` is used for writing blocks to disk.
+
 ```ts
-createBlocks({ input, bucket, blocks }: Deduplicate.CreateBlocksOptions): Promise<void>;
+Dedupe.createBlocks(
+    options: Deduplicate.CreateBlocksOptions
+): Promise<void>;
+```
+
+**Example**
+
+```ts
+await createBlocks({
+  input: filePath,
+  bucket: directoryToSaveBlocks,
+  blocks: info.blocks
+});
 ```
--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,103 +1,125 @@
-import { Dedupe, deduplicate, mergeBlocks, getHash, createBlocks, validateBlocks, validateBlock } from '../src'
-import { stat, ensureDir, pathExists, truncate, rename, removeSync } from 'fs-extra'
-import { join } from 'path'
-const file = join(__dirname, 'file.zip'),
-  output = join(__dirname, 'new-file.zip'),
-  bucket = join(__dirname, 'bucket')
-let fileSize: number, blocks: Dedupe.FileDedupeObject
+import {
+  Dedupe,
+  deduplicate,
+  mergeBlocks,
+  getHash,
+  createBlocks,
+  validateBlocks,
+  validateBlock
+} from "../src";
+import {
+  stat,
+  ensureDir,
+  pathExists,
+  truncate,
+  rename,
+  removeSync
+} from "fs-extra";
+import { join } from "path";
+const file = join(__dirname, "file.zip"),
+  output = join(__dirname, "new-file.zip"),
+  bucket = join(__dirname, "bucket");
+let fileSize: number, blocks: Dedupe.FileDedupeObject;
 
 describe("Generate file hash and it's blocks", () => {
   beforeAll(async () => {
-    await ensureDir(bucket)
-  })
-  test('deduplicate file', async done => {
-    fileSize = (await stat(file)).size
-    blocks = await deduplicate(file)
-    expect(blocks).not.toBeUndefined()
-    done()
-  })
+    await ensureDir(bucket);
+  });
+  it("should deduplicate the file", async done => {
+    fileSize = (await stat(file)).size;
+    blocks = await deduplicate(file);
+    expect(blocks).not.toBeUndefined();
+    done();
+  });
 
-  test('re-deduplicate file', done => {
+  it("should re-deduplicate the file and compares the output with the previous output", done => {
     deduplicate(file).then(d => {
-      expect(d).toEqual(blocks)
-      done()
-    })
-  })
-  test('create blocks, re-hash and compare', async done => {
-    removeSync(bucket)
-    await ensureDir(bucket)
-    await createBlocks({ input: file, bucket, blocks: blocks.blocks })
-    const newBlockHashs: string[] = []
+      expect(d).toEqual(blocks);
+      done();
+    });
+  });
+  it("should write file blocks/chunks to disk, re-hash and compare each block/chunk", async done => {
+    removeSync(bucket);
+    await ensureDir(bucket);
+    await createBlocks({ input: file, bucket, blocks: blocks.blocks });
+    const newBlockHashs: string[] = [];
     for (const block of blocks.blocks) {
       const path = join(bucket, block.hash),
         exists = await pathExists(path),
-        h = await getHash(path)
-      expect(true).toEqual(exists)
-      expect(block.hash).toEqual(h)
-      newBlockHashs.push(h)
+        h = await getHash(path);
+      expect(true).toEqual(exists);
+      expect(block.hash).toEqual(h);
+      newBlockHashs.push(h);
     }
-    const blocksHash = blocks.blocks.map(b => b.hash)
-    expect(newBlockHashs).toEqual(blocksHash)
-    done()
-  })
+    const blocksHash = blocks.blocks.map(b => b.hash);
+    expect(newBlockHashs).toEqual(blocksHash);
+    done();
+  });
 
-  test('rerun (create blocks, re-hash and compare) without passing blocks', async done => {
-    removeSync(bucket)
-    await ensureDir(bucket)
-    await createBlocks({ input: file, bucket })
-    const newBlockHashs: string[] = []
+  it("rerun (create blocks, re-hash and compare) without passing blocks", async done => {
+    removeSync(bucket);
+    await ensureDir(bucket);
+    await createBlocks({ input: file, bucket });
+    const newBlockHashs: string[] = [];
     for (const block of blocks.blocks) {
       const path = join(bucket, block.hash),
         exists = await pathExists(path),
-        h = await getHash(path)
-      expect(true).toEqual(exists)
-      expect(block.hash).toEqual(h)
-      newBlockHashs.push(h)
+        h = await getHash(path);
+      expect(true).toEqual(exists);
+      expect(block.hash).toEqual(h);
+      newBlockHashs.push(h);
     }
-    const blocksHash = blocks.blocks.map(b => b.hash)
-    expect(newBlockHashs).toEqual(blocksHash)
-    done()
-  })
+    const blocksHash = blocks.blocks.map(b => b.hash);
+    expect(newBlockHashs).toEqual(blocksHash);
+    done();
+  });
 
-  test('Merge file blocks back', async done => {
+  it("Merge file blocks back", async done => {
     const op: Dedupe.MergeOptions = {
       output,
-      blocks: blocks.blocks.map(b => ({ start: b.start, end: b.end, bucket, hash: b.hash }))
-    }
-    await mergeBlocks(op)
-    const newSize = (await stat(output)).size
-    expect(newSize).toEqual(fileSize)
-    const newBlocks = await deduplicate(output)
-    expect(newBlocks).toEqual(blocks)
-    done()
-  })
-  test('Validate a block', async done => {
-    let b = blocks.blocks[0]
-    let block = { size: b.end - b.start, hash: b.hash }
-    let isValid = await validateBlock(bucket, block)
-    expect(isValid).toEqual(true)
-    done()
-  })
-  test('Validate if file blocks are truly written to bucket', async done => {
-    let isValid = await validateBlocks(bucket, blocks)
-    expect(isValid).toEqual(true)
+      blocks: blocks.blocks.map(b => ({
+        start: b.start,
+        end: b.end,
+        bucket,
+        hash: b.hash
+      }))
+    };
+    await mergeBlocks(op);
+    const newSize = (await stat(output)).size;
+    expect(newSize).toEqual(fileSize);
+    const newBlocks = await deduplicate(output);
+    expect(newBlocks).toEqual(blocks);
+    done();
+  });
+  it("Validate a block", async done => {
+    let b = blocks.blocks[0];
+    let block = { size: b.end - b.start, hash: b.hash };
+    let isValid = await validateBlock(bucket, block);
+    expect(isValid).toEqual(true);
+    done();
+  });
+  it("Validate if file blocks are truly written to bucket", async done => {
+    let isValid = await validateBlocks(bucket, blocks);
+    expect(isValid).toEqual(true);
     // check for missing path
-    removeSync(join(bucket, blocks.blocks[3].hash))
-    isValid = await validateBlocks(bucket, blocks)
+    removeSync(join(bucket, blocks.blocks[3].hash));
+    isValid = await validateBlocks(bucket, blocks);
 
-    expect(isValid).toEqual(false)
+    expect(isValid).toEqual(false);
     // Check for block size
-    await truncate(join(bucket, blocks.blocks[2].hash))
-    isValid = await validateBlocks(bucket, blocks)
-    expect(isValid).toEqual(false)
+    await truncate(join(bucket, blocks.blocks[2].hash));
+    isValid = await validateBlocks(bucket, blocks);
+    expect(isValid).toEqual(false);
     // Check for wrong content
-    await rename(join(bucket, blocks.blocks[1].hash), join(bucket, blocks.blocks[0].hash))
-    isValid = await validateBlocks(bucket, blocks)
-    expect(isValid).toEqual(false)
-    done()
-  })
-  test('Throw error if validateBlocks fileObject is not passed', async () => {
-    await expect(validateBlocks(bucket, null)).rejects.toThrow()
-  })
-
-})
+    await rename(
+      join(bucket, blocks.blocks[1].hash),
+      join(bucket, blocks.blocks[0].hash)
+    );
+    isValid = await validateBlocks(bucket, blocks);
+    expect(isValid).toEqual(false);
+    done();
+  });
+  it("Throw error if validateBlocks fileObject is not passed", async () => {
+    await expect(validateBlocks(bucket, null)).rejects.toThrow();
+  });
+});
